8587a8177f82e59d2269a60d1849dfdc917b167f
Luan Olivo <luanolivo@outlook.com>
2022-12-19 19:27:56 -0300

    update

---
+++ js/questions.js
@@ -0,0 +1,59 @@
+let questions = [
+    {
+        question: 'Pergunta 1... ?',
+        options: [
+            "opção um",
+            "opção dois",
+            "opção tres",
+            "opção quatro"
+        ], 
+        answer: 1
+    },
+
+    {
+        question: 'Pergunta 2... ?',
+        options: [
+            "opção um",
+            "opção dois",
+            "opção tres",
+            "opção quatro"
+        ], 
+        answer: 2
+    },
+
+    {
+        question: 'Pergunta 3... ?',
+        options: [
+            "opção um",
+            "opção dois",
+            "opção tres",
+            "opção quatro"
+        ], 
+        answer: 3
+    },
+
+    {
+        question: 'Pergunta 4... ?',
+        options: [
+            "opção um",
+            "opção dois",
+            "opção tres",
+            "opção quatro"
+        ], 
+        answer: 4
+    },
+
+    {
+        question: 'Pergunta 5... ?',
+        options: [
+            "opção um",
+            "opção dois",
+            "opção tres",
+            "opção quatro"
+        ], 
+        answer: 5
+    },
+
+   
+]
+console.log(questions)
